74f9f1ff2309773b659f1cdd6610f2c7a88fa8cd
Folke Forshed <folkefd@gmail.com>
2023-01-11 11:47:57 +0100

    chatgpt_api now works, you can call get_response(prompt) and it will return ans

--- chatgpt_api.py
+++ chatgpt_api.py
@@ -1,38 +1,28 @@
+import openai
 import json
-import requests
 
-# Import configurations
 import config
 
 
 class ChatGptAPI:
     def __init__(self):
-        engine = "davinci"
-        self.base_url = f"https://api.openai.com/v1/engines/{engine}/completions"
-        self.headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {config.CHATGPT_TOKEN}"
-        }
+        self.model = "text-davinci-003"
+        self.token = config.CHATGPT_TOKEN
         self.options = {
-            "max_tokens": 100,
-            "stop": "END_OF_UTTERANCE",
+            "max_tokens": 50,
             "temperature": 0.5
         }
+        openai.api_key = self.token
 
     def generate_response(self, prompt: str):
-        data = {
-            "prompt": prompt,
-            "options": self.options
-        }
         try:
-            response = requests.post(self.base_url, headers=self.headers, json=data)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as err:
-            if response.status_code == 401:
-                raise ValueError("Failed to generate response. Invalid or expired token.")
-            else:
-                raise ValueError(f"Failed to generate response. {err}")
-        except requests.exceptions.RequestException as err:
-            raise ValueError(f"Failed to generate response. {err}")
-
-        return json.loads(response.text)
+            response = openai.Completion.create(
+                model=self.model,
+                prompt=prompt,
+                temperature=self.options["temperature"],
+                max_tokens=self.options["max_tokens"]
+            )
+        except Exception as e:
+            raise ValueError(f"Failed to generate response. {e}")
+        print(response)
+        return response.choices[0].text
